4d75d1878c4c6fc191ba40a03958cb7f3d276e9d
Noor Alrahhal <noor.rahhal03@gmail.com>
2025-04-12 16:59:57 +0300

    Fix base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Password-Generator-App/",
-  esbuild: { loader: "jsx" },
-  resolve: {
-    alias: {
-      "./runtimeConfig": "./runtimeConfig.browser",
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        ".js": "jsx",
-      },
-    },
-  },
+  base: "/",
 });
